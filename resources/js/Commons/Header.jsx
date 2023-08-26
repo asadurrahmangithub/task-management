d350ab48d60ecc3fa0c93cc1dc07a51ef52d5369
asadurrahmangithub <asadurrhman@gmail.com>
2023-08-26 17:39:21 +0600

    new

--- resources/js/Commons/Header.jsx
+++ resources/js/Commons/Header.jsx
@@ -11,6 +11,7 @@ const Header = ({ onSearch }) => {
     const handleSearch = () => {
         onSearch(searchQuery);
     };
+    
 
     return (
         <nav className="navbar navbar-expand-lg">
